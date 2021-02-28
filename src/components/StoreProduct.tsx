c7a44d84faf11552ac4cc791c45479633e732d23
Marsunpaisti <marsunpaisti@gmail.com>
2021-02-28 22:37:21 +0200

    aaaaagh

--- src/components/StoreProduct.tsx
+++ src/components/StoreProduct.tsx
@@ -5,13 +5,15 @@ export interface StoreProductProps {
     title: string;
     image: string;
     description: string;
+    price?: string;
 }
-export const StoreProduct: React.FC<StoreProductProps> = ({title, image, description}) => {
+export const StoreProduct: React.FC<StoreProductProps> = ({title, image, description, price}) => {
 	return (
         <div className={css.container}>
             <img src={image} alt={title}/>
             <span className={css.title}>{title}</span>
             <span className={css.description}>{description}</span>
+            <span className={css.price}>{price ? price : 'FREE'}</span>
         </div>
     )
 };
--- src/pages/StorePage.tsx
+++ src/pages/StorePage.tsx
@@ -1,29 +1,22 @@
 import React from 'react';
 import { StoreProduct } from '../components/StoreProduct';
 import css from './StorePage.module.scss';
+import productJson from '../products.json'
+
 export const StorePage = () => {
-
-    const product = { 
-        title: 'AIO Fighter',
-        description: 'Free version of PAIOFighter that will automatically fight enemies for you',
-        image: 'https://firebasestorage.googleapis.com/v0/b/paistiplugins/o/fighter.png?alt=media&token=bf30c36b-b85d-4af5-bf68-7460e88e797d',
-    }
-
+    const products = productJson;
     return (
         <>
             <div className={css.categoryContainer}>
                 <span className={css.categoryTitle}>Free plugins</span>
                 <div className={css.productsContainer}>
-                    <StoreProduct {...product}/>
-                    <StoreProduct {...product}/>
+                    {products.filter(p => !p.price).map(p => <StoreProduct {...p} key={p.title}/>)}
                 </div>
             </div>
             <div className={css.categoryContainer}>
                 <span className={css.categoryTitle}>Premium plugins</span>
                 <div className={css.productsContainer}>
-                    <StoreProduct {...product}/>
-                    <StoreProduct {...product}/>
-                    <StoreProduct {...product}/>
+                    {products.filter(p => p.price).map(p => <StoreProduct {...p} key={p.title}/>)}
                 </div>
             </div>
         </>
